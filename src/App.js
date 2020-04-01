5073c1a8910eb35e5feddb5d32064bacb1481b20
Alexandra31150 <alexandrap44@gmail.com>
2020-04-01 10:02:49 +0200

    modif App.js suite correction en appellant Contact

--- src/App.js
+++ src/App.js
@@ -6,18 +6,7 @@ import "./components/contact.css";
 function App() {
   return (
     <div className="Contact">
-      <img
-        class="avatar"
-        src="https://randomuser.me/api/portraits/women/91.jpg"
-        alt=""
-      />
-      <div class="name">
-        Tara Curtis
-        <div class="status">
-          <span class="status-online"></span>
-          <p class="status-text">online</p>
-        </div>
-      </div>
+      <Contact />
     </div>
   );
 }
